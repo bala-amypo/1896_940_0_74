5a526b769d00f2640cb534e344ae819c55f02463
bala-amypo <bala@amypo.in>
2025-12-19 08:12:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,49 +1,35 @@
-package com.example.demo.entity;
 
-import java.time.LocalDate;
+package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.*;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Student {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
+
+    @NotBlank(message = "Name field cannot be empty")
+    @Size(min = 3, max = 20, message = "The user name must be min of 3 and max of 20 characters")
     private String name;
+
+    @Email(message = "Invalid email id")
+    @NotBlank(message = "Email cannot be empty")
+    @Column(unique = true)
     private String email;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    
-    }
-    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
 
-    }
-    public Student(){
-
-    }
 }
--- src/main/java/com/example/demo/serviceimpl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceimpl.java
@@ -1,4 +1,6 @@
-package com.example.demo.Serviceimpl;
+
+
+package com.example.demo.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,22 +8,32 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
+import com.example.demo.exception.ResourceNotFoundException;
+
 
 @Service
-public class StudentServiceImple implements StudentService  {
- 
-
-    // private final StudentRepository studentRepository;
-    // public StudentServiceImple(StudentRepository studentRepository) {
-    //     this.studentRepository = studentRepository;
-    // }
- 
+public class StudentServiceImpl implements StudentService {
     @Autowired
     StudentRepository studentRepository;
 
-    public Student saveStudent(Student student) {
-        return studentRepository.save(student);// Saving student details using repository save method/ Ins
-    
-
+    public Student saveStudent(Student student){
+        return studentRepository.save(student);
+    }
+    public Student getStudentById(Long id){
+        return studentRepository.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+    public Student updatedata(Long id, Student student){
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRepository.save(exits);
+        // .orElseThrow(()-> new ResourceNotFoundException("Invalid Email"));
+    }
+    public Student deletedata(Long id){
+        Student student=getStudentById(id);
+        studentRepository.delete(student);
+        return student;
     }
 }
+
